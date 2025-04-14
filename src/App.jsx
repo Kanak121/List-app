26572e74f775bdbd84689a9395cb51613743f75a
kanak <kanak@gmail.com>
2025-04-14 16:04:49 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+// import Tswitch from './tSwitch';
+import List from './list';
+function App() {
+  return (
+   <>
+   {/* <Tswitch/> */}
+   <List/>
+   </>
+  );
+}
+
+export default App;
---
+++ src/list.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import React from 'react'
+import './list.css';
+import { IoAdd } from "react-icons/io5";
+import { MdAutoDelete } from "react-icons/md";
+
+const List = () => {
+    const [inputValue,setInputValue]=useState(" ");
+    const[setData,setNewData]=useState([]);
+    
+
+
+    const handleInputChange=(value)=>{
+      setInputValue(value);
+    };
+
+  const handleRefresh=(event)=>{
+    event.preventDefault();
+
+    if(!inputValue) return;
+
+    if(setData.includes(inputValue)) {
+      // setInputValue("")
+      return;
+    }
+
+    setNewData((prevdata)=> [...prevdata,inputValue])
+
+    setInputValue("")
+  };
+
+
+
+
+   
+  return (
+    <section className='container'>
+        <header>
+        <h1>Todo-List</h1>
+        </header>
+        <section className='form'>
+        <form onSubmit={handleRefresh} >
+            <div>
+        <input 
+        type = "text"
+        className='todo-input'
+        autoComplete='off'
+        value = {inputValue}
+        onChange={(event)=>handleInputChange(event.target.value)}
+         />
+        <button type ="submit" className='todo-btn'>Add Task</button>
+        </div>
+        </form>
+        
+        </section>
+        <section className='unorder-list'>
+           <ul>
+            {
+              setData.map((curElem,index)=>{
+                return(
+                  <li key={index}>
+                     <span> {curElem}</span>
+                     <button className='add-btn'>
+                     <IoAdd />
+                     </button>
+                     <button className='delete-btn'>
+                     <MdAutoDelete />
+
+                     </button>
+                  </li>
+                )
+              })
+            }
+           </ul>
+        </section>
+    </section>
+  )
+}
+
+export default List;
